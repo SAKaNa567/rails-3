0684b9580d1543c392c477d284e39573f8d879d5
naoto <s1240147@u-aizu.ac.jp>
2018-07-03 01:22:43 +0900

    @base_title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,9 +2,12 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  test "should get root" do
-    get FILL_IN
-    assert_response FILL_IN
+  # test "should get root" do
+  #   get FILL_IN
+  #   assert_response FILL_IN
+  # end
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get home" do
